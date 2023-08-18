56d19196603cdc63517d948f64e232d15a5c1c1f
Kythonlk <kythonlk@gmail.com>
2023-08-19 00:41:38 +0530

    first commit to git

---
+++ components/alert.tsx
@@ -0,0 +1,11 @@
+import Container from './container'
+import cn from 'classnames'
+import { EXAMPLE_PATH } from '../lib/constants'
+
+export default function Alert({ preview }) {
+  return (
+    <>
+     
+    </>
+  )
+}
---
+++ components/footer.tsx
@@ -0,0 +1,30 @@
+import Container from './container'
+import { GITHUB_URI , DEV_NAME } from '../lib/constants'
+
+export default function Footer() {
+  return (
+    <footer className="bg-accent-1 border-t border-accent-2">
+      <Container>
+        <div className="py-28 flex flex-col lg:flex-row items-center">
+          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            Statically Generated with Next.js.
+          </h3>
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+            <a
+              href="https://kythonlk.com"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+            >
+              Developed by {DEV_NAME}
+            </a>
+            <a
+              href={`${GITHUB_URI}`}
+              className="mx-3 font-bold hover:underline"
+            >
+             View source code on GitHub
+            </a>
+          </div>
+        </div>
+      </Container>
+    </footer>
+  )
+}

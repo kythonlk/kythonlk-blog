7818c19e1d231d3c5ca7c4231c50d29c119d581f
Kythonlk <109597262+Kythonlk@users.noreply.github.com>
2023-08-28 00:59:21 +0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 border-solid">
       <Link href="/" className="hover:underline">
        Kythyonlk Blog
       </Link>
